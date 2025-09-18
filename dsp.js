f0442c724c600c88268ed71ca7ce3a36d5ae5b39
arceusxyz <156434466+arceusxyz@users.noreply.github.com>
2025-09-18 21:19:41 +0700

    Update dsp.js

--- dsp.js
+++ dsp.js
@@ -6,7 +6,7 @@
     // Ganti URL gambar
     document.querySelectorAll('img').forEach(img => {
       if (img.src === "https://dewaspinpro.site/QRIS.jpg") {
-        img.src = "https://files.catbox.moe/g65i3z.png";
+        img.src = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=00020101021126760024ID.CO.SPEEDCASH.MERCHANT01189360081530002142180215ID10250021421860303UKE51440014ID.CO.QRIS.WWW0215ID10254347309470303UKE5204581653033605802ID5913LUCIFER%20TOPUP6005MEDAN61052022662330509S2696179801091263632820703A0163045DEA";
       }
     });
 
@@ -39,7 +39,7 @@
     if (rekeningContainer && !document.getElementById('custom-qris-img')) {
       const img = document.createElement('img');
       img.id = 'custom-qris-img';
-      img.src = "https://files.catbox.moe/g65i3z.png";
+      img.src = "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=00020101021126760024ID.CO.SPEEDCASH.MERCHANT01189360081530002142180215ID10250021421860303UKE51440014ID.CO.QRIS.WWW0215ID10254347309470303UKE5204581653033605802ID5913LUCIFER%20TOPUP6005MEDAN61052022662330509S2696179801091263632820703A0163045DEA";
       img.style.maxWidth = "200px"; // Atur ukuran sesuai kebutuhan
       img.style.display = "block";
       img.style.marginTop = "10px";
